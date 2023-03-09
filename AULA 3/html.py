4250a4e40b1b636dbb7b7811335169785ec6ba06
joanamffernandes <joanaff20@gmail.com>
2023-03-09 11:44:46 +0000

    .

---
+++ AULA 3/html.py
@@ -0,0 +1,45 @@
+# Transformar o ficheiro txt em html
+
+import re
+file = open ('dicionario_medico.txt', encoding='utf8')
+text = file.read()
+
+# remover os ff
+text = re.sub(r'\f', '', text)
+
+entries = re.findall(r'\n\n(.+)((?:\n.+)+)', text)
+
+#rmeover os \n 
+new_entries = [(designation, description.strip()) for designation, description in entries]
+
+
+'''for designation, description in entries:
+    description = description.strip()
+    new_entries.append((designation, description))
+'''
+
+print(new_entries[10])
+
+file.close()
+
+html = open ('dicionario_medico.html', 'w',encoding='utf8' )
+
+header = '''<html>
+<head> 
+<meta charset='utf-8'/>
+</head>
+<body>
+'''
+
+body = ''
+for designation, description in new_entries:
+    body += '<b>' + designation + '</b>'
+    body += '<p>' + description + '</p>' + '<hr>'
+
+footer = ''' </body>
+</html>'''
+
+html.write(header + body + footer)
+
+
+html.close()
